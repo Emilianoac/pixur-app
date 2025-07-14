21188a8e1af6cb7c5c372fba8eca4eb1faeb93b5
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-07-14 15:45:54 -0400

    refactor(SettingsScreen): improve code formatting and structure for readability

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,52 +1,53 @@
-import { View, SafeAreaView, ScrollView, Image } from "react-native"
-import {useState } from "react"
-import {signOut} from "firebase/auth"
-import {auth} from "@/firebaseConfig"
-import {images} from "@/constants/index"
-import FormField from "@/components/FormField"
-import CustomButton from "@/components/CustomButton"
-import Loader from "@/components/Loader"
+import { View, SafeAreaView, ScrollView, Image } from "react-native";
+import {useState } from "react";
+import {signOut} from "firebase/auth";
+import {auth} from "@/firebaseConfig";
+import {images} from "@/constants/index";
+import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButton";
+import Loader from "@/components/Loader";
 import { useAuthStore } from "@/store/useAuthStore";
 
 export default function SettingsScreen() {
   const userData = useAuthStore((state) => state.userData);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   // Sign out user
   async function handleSignOut() {
-    setLoading(true)
-    await signOut(auth)
-    setLoading(false)
+    setLoading(true);
+    await signOut(auth);
+    setLoading(false);
   }
 
   return (
-    <SafeAreaView className="bg-secondary-700 h-full">
-      <ScrollView>
-        <View className="items-center p-4 py-10">
-          <View className="w-full">
-            <View className="flex justify-center w-[150px] h-[150px] items-center border-2 border-slate-700 rounded-full mx-auto">
-              <Image
-                source={images.profileImage}
-                className="w-[140px] h-[140px] rounded-full opacity-60"
-                resizeMode="cover"
-              />
-            </View>
-            <FormField
+    <>
+      <ScrollView className="bg-secondary-700"  keyboardShouldPersistTaps="handled">
+        <View className="w-full flex-1 justify-center items-center px-4 py-10">
+          <View className="flex justify-center w-[150px] h-[150px] items-center border-2 border-slate-700 rounded-full mx-auto">
+            <Image
+              source={images.profileImage}
+              className="w-[140px] h-[140px] rounded-full opacity-60"
+              resizeMode="cover"
+            />
+          </View>
+
+          <View className="mt-4 w-full">
+            <FormField 
               title="Email"
               readOnly={true}
               placeholder="Email"
               value={userData?.email || ""}
               handleChangeText={(text) => {}}
             />
+            <CustomButton
+              title="Sign Out"
+              containerStyle="mt-4 bg-slate-300"
+              handlePress={handleSignOut}
+            />
           </View>
-          <CustomButton
-            title="Sign Out"
-            containerStyle="mt-4 bg-slate-300"
-            handlePress={handleSignOut}
-          />
         </View>
       </ScrollView>
       <Loader isLoading={loading} message="Sign out.." />
-    </SafeAreaView>
+    </>
   )
 }
